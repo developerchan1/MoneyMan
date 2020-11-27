cb7a59790526e36b7243e0e654ff7f9e9b148fab
Chandra <developerchan1@gmail.com>
2020-11-27 22:51:34 +0700

    Add File
    Add Transaction.swift

---
+++ MoneyMan/Model/Transaction.swift
@@ -0,0 +1,18 @@
+//
+//  Transaction.swift
+//  MoneyMan
+//
+//  Created by Jesselyn on 25/11/20.
+//  Copyright © 2020 KelompokMMS. All rights reserved.
+//
+
+import Foundation
+
+struct Transaction {
+    var name:String
+    var date:String
+    var category:String
+    var price:Int
+    var method:String
+    var desc:String
+}
